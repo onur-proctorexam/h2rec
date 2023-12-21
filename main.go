f74aea5991753afa2545be44ecbd8626c0905543
Onur Uyar <onur@proctorexam.com>
2023-12-21 18:40:27 +0100

    change port to aws healtcheck port 8080

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to App Runner</h1>")
 	})
-	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Starting the server on :8080...")
+	http.ListenAndServe(":8080", nil)
 }
